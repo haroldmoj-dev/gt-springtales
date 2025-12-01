dd68e09842a6d6b2aa5aa481bbbc0fb734d9550a
Harold <haroldmojica.dev@gmail.com>
2025-12-01 11:42:30 +0800

    Add hamburger menu

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const rightMenu = document.querySelector(".navbar .right");
+
+  hamburger.addEventListener("click", function (e) {
+    rightMenu.classList.toggle("active");
+    e.stopPropagation();
+  });
+
+  document.addEventListener("click", function (e) {
+    if (!hamburger.contains(e.target) && !rightMenu.contains(e.target)) {
+      rightMenu.classList.remove("active");
+    }
+  });
+});
